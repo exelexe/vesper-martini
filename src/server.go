b7f3ca434c04bf68036bc54ebbcc7ab6485caed2
h-miyamo <hoge@hage.com>
2014-06-16 06:05:56 +0000

    spliting function

--- src/server.go
+++ src/server.go
@@ -2,10 +2,10 @@ package main
 
 import (
     "app"
-    . "core/db"
+    //    . "core/db"
     "github.com/go-martini/martini"
-    "log"
-    "schemas"
+    //    "log"
+    //    "schemas"
 )
 
 func main() {
@@ -13,13 +13,15 @@ func main() {
 
     // route handler
     m.Get("/hello", app.Hello)
-    m.Get("/json/:id", func(params martini.Params) string {
-        var user schemas.User
-        DB.First(&user, params["id"])
-        log.Println(user)
-        //return "Hello " + params["id"]
-        return "name: " + user.Name
-    })
-
+    m.Get("/user/:id", app.FindUser)
+    /*
+       m.Get("/json/:id", func(params martini.Params) string {
+           var user schemas.User
+           DB.First(&user, params["id"])
+           log.Println(user)
+           //return "Hello " + params["id"]
+           return "name: " + user.Name
+       })
+    */
     m.Run()
 }
