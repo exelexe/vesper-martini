aa1d6224b3a40e4d73fbf5a9fe922dcec1ff756a
h-miyamo <deckn.m@gmail.com>
2014-11-03 01:42:55 +0000

    add cors

--- server.go
+++ server.go
@@ -4,12 +4,25 @@ import (
 	"github.com/go-martini/martini"
 	"github.com/codegangsta/martini-contrib/render"
 	"github.com/h-miyamo/vesper-martini/app"
+	"github.com/martini-contrib/cors"
 )
 
 func main() {
 	// setup middleware
 	m := martini.Classic()
 	m.Use(render.Renderer())
+
+	// CORS for http://localhost origins, allowing:
+	// - GET methods
+	// - Origin header
+	// - Credentials share
+	m.Use(cors.Allow(&cors.Options{
+		AllowOrigins:		[]string{"http://localhost"},
+		AllowMethods:		[]string{"GET"},
+		AllowHeaders:		[]string{"Origin"},
+		ExposeHeaders:		[]string{"Content-Length"},
+		AllowCredentials:	true,
+	}))
 
 	// setup routes
 	m.Get("/hello", app.Hello)
